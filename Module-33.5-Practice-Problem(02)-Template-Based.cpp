563ef7522e26b45bfe5951bc02795b2dacf27510
IEEEMOSTAFA <121746399+IEEEMOSTAFA@users.noreply.github.com>
2023-05-19 00:31:24 +0600

    Add files via upload

---
+++ Module-33.5-Practice-Problem(02)-Template-Based.cpp
@@ -0,0 +1,142 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+template<class T>
+class MinHeap{
+public:
+    vector<T>nodes;
+
+    MinHeap()
+    {
+
+
+    }
+
+    //O(logn)
+    void up_heapify(T idx)
+    {
+        while(idx > 0 && nodes[idx] > nodes[(idx-1)/2])
+        {
+            swap(nodes[idx] , nodes[(idx-1)/2]);
+            idx = (idx-1)/2;
+        }
+    }
+    //O(logn)
+    void insert(T x)
+    {
+        nodes.push_back(x);
+        up_heapify(nodes.size()-1);
+    }
+
+    //O(n)
+    void PrintHeap()
+    {
+        for(int i=0;i<nodes.size();i++)
+        {
+            cout<<nodes[i]<<" ";
+        }
+        cout<<"\n";
+    }
+
+    //O(logn)
+    void down_heapify(T idx)
+    {
+        while(1)
+        {
+            int largest = idx;
+            int l = 2*idx + 1;
+            int r = 2*idx + 2;
+            if(l<nodes.size() && nodes[largest] > nodes[l])
+            {
+                largest = l;
+            }
+            if(r<nodes.size() && nodes[largest] > nodes[r])
+            {
+                largest = r;
+            }
+            if(largest==idx)
+                break;
+            swap(nodes[idx] , nodes[largest]);
+            idx = largest;
+        }
+
+    }
+
+    //O(logn)
+    void Delete(T idx)
+    {
+        if(idx >= nodes.size())
+            return;
+        swap(nodes[idx] , nodes[nodes.size()-1]);
+        nodes.pop_back();
+        down_heapify(idx);
+    }
+
+    //O(1)
+    int getMax()
+    {
+        if(nodes.empty())
+        {
+            cout<<"Heap is empty!\n";
+            return -1;
+        }
+        return nodes[0];
+    }
+
+    //O(logn)
+    int ExtractMax()
+    {
+        if(nodes.empty())
+        {
+            cout<<"Heap is empty!\n";
+            return -1;
+        }
+        int ret = nodes[0];
+        Delete(0);
+        return ret;
+    }
+
+    //O(n)
+    void build_from_array(vector<T>&a)
+    {
+        nodes = a;
+        int n = nodes.size();
+        int last_non_leaf = n/2 - 1;
+        for(int i=last_non_leaf; i>=0; i--)
+        {
+            down_heapify(i);
+        }
+    }
+};
+
+//O(nlogn)
+/*
+vector<int> heap_sort(vector<int>a)
+{
+    MaxHeap h;
+    h.build_from_array(a);
+    vector<int>ans;
+    for(int i=0;i<a.size();i++)
+    {
+        ans.push_back(h.ExtractMax());
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
+*/
+int main()
+{
+   //MaxHeap h;
+   //<int>h;
+   MinHeap<int>h;
+   vector<int>a = {1,2,3,4,10,9,8,7};
+   h.build_from_array(a);
+    h.PrintHeap();
+
+
+
+
+    return 0;
+}
+
